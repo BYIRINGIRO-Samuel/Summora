17d1a6162c34566952ec55a94bb75004a8349d06
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-04-17 20:14:02 +0100

    Update home page for grow utility

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#f1f4f9]">
-      <main className="flex-grow">
+    <div className="flex flex-col min-h-screen bg-white">
+      <main className="grow">
         <Hero />
-        {/* Further sections can be added here */}
       </main>
     </div>
   );
